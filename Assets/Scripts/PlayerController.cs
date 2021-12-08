35fb51715d288efd0ce5b2115fd854b639579297
Phoque <lafont_jordan@hotmail.fr>
2021-12-08 12:01:18 +0100

    fixed camera with cinemachine framing transposer + camera collider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -63,37 +63,44 @@ public class PlayerController : MonoBehaviour
 				currentSpeed = walkSpeed; 
 			}
 		}
-		if (inputs.camera != Vector2.zero)
-		{
-			Vector3 camRotation = cameraFollow.rotation.eulerAngles;
-			Vector3 newCamRotation = new Vector3(Mathf.Clamp(camRotation.x + inputs.camera.y, -60f, 60f), camRotation.y + inputs.camera.x, 0f);
-			cameraFollow.rotation = Quaternion.Euler(newCamRotation);
-			//cameraFollow.rotation *= Quaternion.Euler((Vector3)inputs.camera);
-			//cameraFollow.rotation *= Quaternion.AngleAxis(inputs.camera.x, t.up);
-			//cameraFollow.rotation *= Quaternion.AngleAxis(inputs.camera.x, Vector3.up);
-			//cameraFollow.rotation *= Quaternion.AngleAxis(inputs.camera.y, Vector3.right);
-			//cameraFollow.rotation *= Quaternion.Euler((Vector3)inputs.camera);
-			//cameraFollow.rotation *= Quaternion.Euler(inputs.camera.x, inputs.camera.y, 0f);
-			//cameraFollow.Rotate(cameraFollow.up, inputs.camera.x);
-			//cameraFollow.Rotate(cameraFollow.right, inputs.camera.y);
-			//cameraFollow.Rotate(new Vector3(inputs.camera.y, inputs.camera.x, 0f));
-			//cameraFollow.Rotate(new Vector3(inputs.camera.y, inputs.camera.x, 0f));
-		}
+		//if (inputs.camera != Vector2.zero)
+		//{
+		//	cameraFollow.rotation *= Quaternion.AngleAxis(inputs.camera.x, Vector3.up);
+		//	cameraFollow.rotation *= Quaternion.AngleAxis(inputs.camera.y, Vector3.right);
+		//	Vector3 camRotation = cameraFollow.localEulerAngles;
+		//	if (camRotation.x > 180f && camRotation.x < 340f)
+		//	{
+		//		camRotation.x = 340f;
+		//	}
+		//	else if (camRotation.x < 180f && camRotation.x > 40f)
+		//	{
+		//		camRotation.x = 40f;
+		//	}
+		//	camRotation.z = 0f;
+		//	cameraFollow.localEulerAngles = camRotation;
+		//}
 		Debug.DrawRay(t.position, Vector3.down * rayLength, Color.red);
 	}
 
 	void FixedUpdate()
 	{
 
-		rb.velocity = new Vector3(inputs.movement.x * currentSpeed * t.right.x, rb.velocity.y, inputs.movement.y * currentSpeed * t.forward.z);
+		Vector3 moveX = t.right * inputs.movement.x * currentSpeed;
+		Vector3 moveZ = t.forward * inputs.movement.y * currentSpeed;
+		Vector3 move = moveX + moveZ;
+		rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
+		//Debug.Log($"FORWARD : {t.forward}, RIGHT : {t.right}, MOVEX : {moveX}, MOVEY : {moveY}, MOVE : {move}, VELOCITY : {rb.velocity}");
+		//rb.velocity = new Vector3(inputs.movement.x * currentSpeed * t.right.x, rb.velocity.y, inputs.movement.y * currentSpeed * t.forward.z);
 		if (inputs.movement != Vector2.zero)
 		{
 			Vector3 cameraForward = Camera.main.transform.forward;
+			cameraForward = new Vector3(cameraForward.x, 0f, cameraForward.z);
 			rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, Quaternion.LookRotation(cameraForward), rotationSpeed));
-		}
-		if (inputs.camera != Vector2.zero)
-		{
-			//TODO
+			//cameraFollow.rotation = Quaternion.Euler(cameraFollow.rotation.eulerAngles.x, 0f, 0f);
+
+			//Vector3 cameraAngles = cameraFollow.localEulerAngles;
+			//transform.rotation = Quaternion.Euler(0, cameraFollow.eulerAngles.y, 0);
+			//cameraFollow.localEulerAngles = new Vector3(cameraAngles.x, 0, 0);
 		}
 		CheckGround();
 		if (inputs.jump && isGrounded)
